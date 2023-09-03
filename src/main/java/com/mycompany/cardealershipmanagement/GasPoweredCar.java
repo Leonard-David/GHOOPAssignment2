fe7154827a7a7e2f0e442d572c464a4e161fe8e6
Leonard <111067138+Leonard-David@users.noreply.github.com>
2023-09-03 15:17:39 +0200

    Update GasPoweredCar.java

--- src/main/java/com/mycompany/cardealershipmanagement/GasPoweredCar.java
+++ src/main/java/com/mycompany/cardealershipmanagement/GasPoweredCar.java
@@ -11,9 +11,9 @@ public class GasPoweredCar extends Car{
     private double mileage;
     private String fuelType;
     public GasPoweredCar(String carCode, String brand, String model, String engineType,
-            double mileage, String fuelType, String carType, String color, double cost, Date dateBroughtIn){
+            double mileage, String fuelType, String carType, String color, double cost, String dateBroughtIn, String timeBroughtIn){
         
-        super(carCode, brand, model, engineType, mileage, carType, color, cost, dateBroughtIn);
+        super(carCode, brand, model, engineType, mileage, carType, color, cost, dateBroughtIn, timeBroughtIn);
         this.cost = cost;
         this.mileage = mileage;
         this.fuelType = fuelType;
@@ -24,7 +24,7 @@ public class GasPoweredCar extends Car{
         this.cost = cost - (mileage*2);
         return cost;
     }
-     public String getfuelType(){
+     public String getFuelType(){
         return fuelType;
     }
 }
