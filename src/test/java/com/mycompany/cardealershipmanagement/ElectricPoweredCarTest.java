2f1e9bfa4959833e1b4587e19bf36980885c00b2
Leonard <111067138+Leonard-David@users.noreply.github.com>
2023-09-03 15:22:21 +0200

    Update ElectricPoweredCarTest.java

--- src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
+++ src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
@@ -1,7 +1,9 @@
 package com.mycompany.cardealershipmanagement;
 
 import java.text.NumberFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,12 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Group H
  */
 public class ElectricPoweredCarTest {
-    Date  date = new Date();
+    
+      LocalTime cTime = LocalTime.now();
+      LocalDate cDate = LocalDate.now();
+      DateTimeFormatter toTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+      DateTimeFormatter toDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      String currenntTime = toTime.format(cTime);
+      String currentDate = toDate.format(cDate);
+      
     NumberFormat toCurrency = NumberFormat.getCurrencyInstance();
     ElectricPoweredCar electricPoweredCarInstance = null;
     public ElectricPoweredCarTest() {
         electricPoweredCarInstance = new ElectricPoweredCar("5YJSA1CN8D","Tesla","Roadstar","3-phase, 4-pole, induction electric motor",2000.50
-                ,"Electric-Powered-Car","Red",3400000.0,date);
+                ,"Electric-Powered-Car","Red",3400000.0,currentDate,currenntTime);
     }
     
     /**
