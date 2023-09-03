c8c312d778a317fc993667773e3e4660eb2736fd
Leonard <111067138+Leonard-David@users.noreply.github.com>
2023-09-03 20:51:26 +0200

    Update ElectricPoweredCarTest.java

--- src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
+++ src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
@@ -1,41 +1,79 @@
 package com.mycompany.cardealershipmanagement;
 
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author Group H
+ * @author mpula
  */
-public class ElectricPoweredCarTest {
-    
-      LocalTime cTime = LocalTime.now();
-      LocalDate cDate = LocalDate.now();
-      DateTimeFormatter toTime = DateTimeFormatter.ofPattern("HH:mm:ss");
-      DateTimeFormatter toDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-      String currenntTime = toTime.format(cTime);
-      String currentDate = toDate.format(cDate);
-      
-    NumberFormat toCurrency = NumberFormat.getCurrencyInstance();
-    ElectricPoweredCar electricPoweredCarInstance = null;
-    public ElectricPoweredCarTest() {
-        electricPoweredCarInstance = new ElectricPoweredCar("5YJSA1CN8D","Tesla","Roadstar","3-phase, 4-pole, induction electric motor",2000.50
-                ,"Electric-Powered-Car","Red",3400000.0,currentDate,currenntTime);
+public class CustomerTest {
+     Customer customerInstance;
+     License licenseInstance = null;
+    public CustomerTest() {
+        licenseInstance = new License("999999ABC","C1E","01/02/2022","01/02/2026");
+        customerInstance = new Customer("01010700607", "Leonard","David", 'M',"0812883053",licenseInstance);
     }
     
     /**
-     * Test of getCost method, of class ElectricPoweredCar...
+     * Test of getIdNumber method, of class Customer...
      */
     @Test
-    public void testGetCost() {
-        System.out.println("getCost");
-        
-        String expResult = toCurrency.format(3391998.00);
-        String result = toCurrency.format(electricPoweredCarInstance.getCost());
+    public void testGetIdNumber() {
+        System.out.println("getIdNumber");
+        String expResult = "01010700607";
+        String result = customerInstance.getIdNumber();
+        assertEquals(expResult, result);
+   }
+
+    /**
+     * Test of getFirstName method, of class Customer.
+     */
+    @Test
+    public void testGetFirstName() {
+        System.out.println("getFirstName");
+        String expResult = "Leonard";
+        String result = customerInstance.getFirstName();
         assertEquals(expResult, result);
     }
+
+    /**
+     * Test of getLastName method, of class Customer.
+     */
+    @Test
+    public void testGetLastName() {
+        System.out.println("getLastName");
+        String expResult = "David";
+        String result = customerInstance.getLastName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCellphoneNo method, of class Customer.
+     */
+    @Test
+    public void testGetCellphoneNo() {
+        System.out.println("getCellphoneNo");
+        String expResult = "0812883053";
+        String result = customerInstance.getCellphoneNo();
+        assertEquals(expResult, result);
+     }
+    @Test
+    public void testGetGender(){
+        System.out.println("getGender");
+        char expResult = 'M';
+        char result = customerInstance.getGender();
+         assertEquals(expResult, result);
+     }
+    /**
+     * Test of getLicense method, of class Customer.
+     */
+    @Test
+    public void testGetLicense() {
+        System.out.println("getLicense");
+        License expResult = licenseInstance;
+        License result = customerInstance.getLicense();
+        assertEquals(expResult, result);
+    }
+    
 }
