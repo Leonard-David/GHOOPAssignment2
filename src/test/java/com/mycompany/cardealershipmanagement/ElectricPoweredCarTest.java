0ec4cf47b0552d6547fc7058f734d492794f1672
Leonard <111067138+Leonard-David@users.noreply.github.com>
2023-09-03 20:53:34 +0200

    Update ElectricPoweredCarTest.java

--- src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
+++ src/test/java/com/mycompany/cardealershipmanagement/ElectricPoweredCarTest.java
@@ -4,7 +4,6 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,44 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Group H
  */
-public class GasPoweredCarTest { 
-     Date date = new Date();
-     NumberFormat toCurrency = NumberFormat.getCurrencyInstance();
-     GasPoweredCar gasPoweredCarInstance = null;
-     
+public class ElectricPoweredCarTest {
+    
       LocalTime cTime = LocalTime.now();
       LocalDate cDate = LocalDate.now();
       DateTimeFormatter toTime = DateTimeFormatter.ofPattern("HH:mm:ss");
       DateTimeFormatter toDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-      String currentTime = toTime.format(cTime);
+      String currenntTime = toTime.format(cTime);
       String currentDate = toDate.format(cDate);
       
-    public GasPoweredCarTest() {
-        gasPoweredCarInstance = new GasPoweredCar("1HGB41JXMN109186","Ford","Mustang-GT","5038 cc (307 cu in) V8",2000.50,
-                "Petrol","Gas-Powered-Car","Dark Matter Gray Metallic",1194000.0,currentDate,currentTime);
+    NumberFormat toCurrency = NumberFormat.getCurrencyInstance();
+    ElectricPoweredCar electricPoweredCarInstance = null;
+    public ElectricPoweredCarTest() {
+        electricPoweredCarInstance = new ElectricPoweredCar("5YJSA1CN8D","Tesla","Roadstar","3-phase, 4-pole, induction electric motor",2000.50
+                ,"Electric-Powered-Car","Red",3400000.0,currentDate,currenntTime);
     }
     
-     /**
-     * Test of getCost method, of class GasPoweredCar...
+    /**
+     * Test of getCost method, of class ElectricPoweredCar...
      */
     @Test
     public void testGetCost() {
         System.out.println("getCost");
-      
-        String expResult = toCurrency.format(1189999.00);
-        String result = toCurrency.format(gasPoweredCarInstance.getCost());
+        
+        String expResult = toCurrency.format(3391998.00);
+        String result = toCurrency.format(electricPoweredCarInstance.getCost());
         assertEquals(expResult, result);
     }
-
-    /**
-     * Test of getfuelType method, of class GasPoweredCar.
-     */
-    @Test
-    public void testGetfuelType() {
-        System.out.println("getfuelType");
-        String expResult = "Petrol";
-        String result = gasPoweredCarInstance.getFuelType();
-        assertEquals(expResult, result);
-   }
-    
 }
