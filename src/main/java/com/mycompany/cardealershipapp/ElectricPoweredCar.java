fa5e69be1f0f3eb35d653c42b54e37bcfb82bffd
Leonard <mpulaldavid@gmail.com>
2023-09-13 01:05:31 +0200

    Assignment 2 on: Exception Handling Files & String manipulation

---
+++ src/main/java/com/mycompany/cardealershipapp/ElectricPoweredCar.java
@@ -0,0 +1,23 @@
+package com.mycompany.cardealershipapp;
+/**
+ *
+ * @author Group Leonard M. David
+ */
+public class ElectricPoweredCar extends Car {
+    // declaring attributes 
+      private double cost;
+      private double mileage;
+      
+     public ElectricPoweredCar(String carCode, String brand, String model, 
+             String engineType, double mileage, String carType, String color, double cost, String dateBroughtIn, String timeBroughtIn){
+        super(carCode, brand, model, engineType, mileage, carType, color, cost, dateBroughtIn, timeBroughtIn);
+        this.cost = cost;
+        this.mileage = mileage;
+    }
+     //overriding an inheritted method
+    @Override
+    public double getCost(){
+        this.cost = cost-(mileage*4);
+        return cost;
+    }
+}
